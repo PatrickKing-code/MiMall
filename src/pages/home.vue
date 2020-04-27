c42b0b188b761b43fd4b4137d543c5197e500f40
sun jing lun <beiershen@163.com>
2020-04-27 11:20:32 +0800

    目录结构

---
+++ src/pages/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <nav-header></nav-header>
+      <router-view></router-view>
+      <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavHeader from '../components/NavHeader'
+import NavFooter from '../components/NavFooter'
+export default {
+    name:"home",
+    data() {
+        return {
+            
+        }
+    },
+    components: {
+        NavFooter,
+        NavHeader
+    }
+}
+</script>
+
+<style>
+
+</style>
