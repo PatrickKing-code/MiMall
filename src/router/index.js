dea84e8e12b6188d7b8a65976be51cb6f7f4b008
Patrick <beiershen@163.com>
2020-05-03 20:01:03 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,53 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
   const routes = [
-  
-]
+    {
+      path: "/",
+      name: "home",
+      component: ()=> import('../views/home.vue'),
+      children: [
+        {
+          path: "/index",
+          name: "index",
+          component: ()=> import('../views/index.vue')
+        },
+        {
+          path: "/product/:id",
+          name: "product",
+          component: ()=> import('../views/product.vue')
+        },
+        {
+          path: "/detail/:id",
+          name: "detail",
+          component: ()=> import('../views/detail.vue')
+        }
+      ]
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: ()=> import('../views/cart.vue'),
+      children: [
+        {
+          path: '/list',
+          name: 'order-list',
+          component: ()=> import('../views/orderList.vue')
+        },
+        {
+          path: '/confirm',
+          name: 'order-confirm',
+          component: ()=> import('../views/orderConfirm.vue')
+        },{
+          path: '/pay',
+          name: 'order-pay',
+          component: ()=> import('../views/orderPay.vue')
+        }
+      ]
+    }
+  ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
