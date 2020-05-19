78d5f89a45695b8b56d33e4d868ccf3e2871131d
孙京沦 <sunjinglun@jilinxiangyun.com>
2020-05-19 13:32:57 +0800

    header-main

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -11,6 +11,18 @@
         <a href="javascript:;">购物车</a>
       </div>
     </div>
+    <div class="header-bar-container">
+      <span class="header-container-logo"></span>
+      <div class="container-bar">
+        <a href="javascript:;" v-for="item in headerBar" :key="item.id">{{item.name}}</a>
+      </div>
+      <div class="container-bar-search">
+        <input type="text" />
+        <span class="search-icon">
+          <img src="../assets/images/search.png" alt="">
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -48,6 +60,13 @@ export default {
           name: "下载app",
           url: "https://www.mi.com/appdownload"
         }
+      ],
+      headerBar: [
+        { name: "小米手机" },
+        { name: "红米手机" },
+        { name: "电视" },
+        { name: "笔记本" },
+        { name: "电视" }
       ]
     };
   }
@@ -76,5 +95,65 @@ export default {
       }
     }
   }
+  .header-bar-container {
+    height: 40px;
+    width: $min-width;
+    margin: 0 auto;
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    .header-container-logo {
+      display: inline-block;
+      height: 55px;
+      width: 55px;
+      align-self: center;
+      overflow: hidden;
+      background: url("../assets/images/logo.png");
+      background-color: #ff6620;
+      &:hover {
+        transition: all 0.5s;
+        background-position: 100% 0;
+      }
+    }
+    .container-bar {
+      height: 100%;
+      line-height: 100px;
+      a {
+        padding-right: 50px;
+        color: #333;
+      }
+    }
+    .container-bar-search {
+      height: 50px;
+      &:focus input{
+        border: 1px solid #ff6620;
+      }
+      &:focus span{
+        border: 1px solid #ff6620;
+      }
+      input {
+        width: 240px;
+        height: 100%;
+        outline: none;
+        font-size: 16px;
+        border: 1px solid #e0e0e0;
+      }
+      .search-icon {
+        display: inline-block;
+        height: 50px;
+        width: 50px;
+        .img {
+          height: 20px;
+          width: 20px;
+          background-color: #ff6620;
+        }
+      }
+    }
+  }
 }
 </style>
+
+ 
+
+
+
